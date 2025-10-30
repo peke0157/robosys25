a66a3817a834e9897cab2c94fd9301826bdea665
hiro <hiroto117@Hirotovivobook.localdomain>
2025-10-30 12:59:41 +0900

    class finish

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Hiroto Miura
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,8 +9,18 @@ ng (){
 
 res=0
 
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
